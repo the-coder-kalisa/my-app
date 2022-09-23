8647c05c6560f5043ecf07e9101fa7a817cab018
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-09-23 19:18:25 +0200

    help

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+const hander : NextApiHandler = async(req: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).send("John Doe")
 }
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default hander
